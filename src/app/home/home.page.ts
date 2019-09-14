8c26b2b5a57a247a2aa0cd84d60d4d0791497734
Feira das Profissões 2019 <feira2019@labcomp.usc>
2019-09-14 15:00:45 -0300

    up native

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
+
+  /**
+   * enviaEmail
+   */
+  public openbarcode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     })
+  }
 }
